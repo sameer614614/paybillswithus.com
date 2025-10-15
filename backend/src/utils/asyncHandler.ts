442ae733ae47f9359fdb7d909e6d4d38b51d5e0b
sameer614614 <sameer614614@gmail.com>
2025-10-16 00:23:29 +0500

    Delete backend directory

--- backend/src/utils/asyncHandler.ts
+++
@@ -1,7 +0,0 @@
-import type { RequestHandler } from 'express';
-
-export function asyncHandler<T extends RequestHandler>(handler: T): RequestHandler {
-  return (req, res, next) => {
-    Promise.resolve(handler(req, res, next)).catch(next);
-  };
-}
